dacb4cd9938065bc9dd2e04da59754cee5d7b997
devlim <ywl428@naver.com>
2024-01-23 00:46:37 +0900

    N.10-3

--- front/src/layouts/Header/index.tsx
+++ front/src/layouts/Header/index.tsx
@@ -3,6 +3,17 @@ import './style.css';
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <div id='header'>
+      <div className='header-left'>
+        <div className='header-left-logo-icon'></div>
+        <div className='header-left-logo-text'>Lims Board</div>
+      </div>
+      <div className='header-right'>
+        <div className='header-icon-box'>
+          <div className='header-icon-search-icon'></div>
+        </div>
+        <div className='header-sign-in-button'>로그인</div>
+      </div>
+    </div>
   )
 }
