d0b9081543554e90209c72f9019869fc24ad5df3
devlim <ywl428@naver.com>
2024-01-26 12:54:04 +0900

    N.15-1

--- front/src/views/MyPage/index.tsx
+++ front/src/views/MyPage/index.tsx
@@ -1,8 +1,71 @@
 import React from 'react'
 import './style.css';
 
+//            component            //
+// description: 마이페이지 화면 //
 export default function MyPage() {
+  //            state            //
+
+  //            function            //
+
+  //            event handler            //
+
+  //            component            //
+  // description: 마이페이지 상단 //
+  const MyPageTop = () => {
+
+    //            state            //
+
+    //            function            //
+
+    //            event handler            //
+
+    //            effect            //
+
+    //            render            //
+    return (
+      <div className='my-page-top'>
+        <div className='my-page-top-container'>
+          <div className='my-page-top-profile-box'>
+            <div className='my-page-top-default-profile'></div>
+          </div>
+          <div className='my-page-top-info-box'>
+            <div className='my-page-info-nickname-container'>
+              <div className='my-page-info-nickname'>{'안녕하세요나는디벨림'}</div>
+              <div className='my-page-info-nickname-button'>
+                <div className='my-page-edit-icon'></div>
+              </div>
+            </div>
+            <div className='my-page-info-email'>{'email@email.com'}</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  //            component            //
+  // description: 마이페이지 상단 //
+  const MyPageBottom = () => {
+
+    //            state            //
+
+    //            function            //
+
+    //            event handler            //
+
+    //            effect            //
+
+    //            render            //
+    return (
+      <div className='my-page-bottom'></div>
+    );
+  }
+
+  //            render            //
   return (
-    <div>MyPage</div>
+    <div id='my-page-wrapper'>
+      <MyPageTop />
+      <MyPageBottom />
+    </div>
   )
 }
