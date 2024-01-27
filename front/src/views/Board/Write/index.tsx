449ce21ce01bb08a546e65b7fc9ff03d6e639692
devlim <ywl428@naver.com>
2024-01-27 10:17:22 +0900

    N.16-1

--- front/src/views/Board/Write/index.tsx
+++ front/src/views/Board/Write/index.tsx
@@ -1,7 +1,21 @@
 import React from 'react'
 import './style.css';
 
+//            component           //
+// description: 게시물 쓰기 화면 //
 export default function BoardWrite() {
+
+  //            state           //
+
+  //            function           //
+
+  //            event handler           //
+
+  //            component           //
+
+  //            effect           //
+
+  //            render           //
   return (
     <div id='board-write-wrapper'>
       <div className='board-write-container'>
@@ -11,19 +25,18 @@ export default function BoardWrite() {
         <div className='divider'></div>
         <div className='board-write-content-container'>
           <div className='board-write-content-input-box'>
-            <textarea className='board-write-content-textarea' placeholder='본문을 작생해주세요'></textarea>
+            <textarea className='board-write-content-textarea' placeholder='본문을 작성해주세요'></textarea>
           </div>
           <div className='board-write-content-button-box'>
             <div className='image-upload-button'>
               <div className='image-upload-icon'></div>
             </div>
-            <input type='file' accept='image/*' />
+            <input type='file' accept='image/*' style={{ display: 'none' }}/>
           </div>
         </div>
         <div className='board-write-image-container'>
-          <div className='board-write-image'>
-            <div className='board-write-image-delete-button'></div>
-          </div>
+          <img className='board-write-image'src='https://www.shinsegaegroupnewsroom.com/wp-content/uploads/2017/11/1-1.jpg' />
+          <div className='board-write-image-delete-button'></div>
         </div>
       </div>
     </div>
